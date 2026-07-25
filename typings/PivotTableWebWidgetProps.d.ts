0aa5bc7e4b99d0e3e45fce0dacc57d69d06bf726
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2026-07-25 16:46:33 +0700

    Mendix 11.12

--- typings/PivotTableWebWidgetProps.d.ts
+++ typings/PivotTableWebWidgetProps.d.ts
@@ -3,9 +3,9 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix Widgets Framework Team
  */
-import { CSSProperties } from "react";
-import { ActionValue, DynamicValue, EditableValue, ListValue, Option, ListAttributeValue } from "mendix";
+import { ActionValue, DynamicValue, EditableValue, ListAttributeValue, ListValue, Option } from "mendix";
 import { Big } from "big.js";
+import { CSSProperties } from "react";
 
 export type DataSourceTypeEnum = "datasource" | "serviceCall";
 
